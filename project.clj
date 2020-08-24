e9c70f68e892f5d8a31d2ad4e6c5625604666d59
Gordon Syme <gordonsyme@gmail.com>
2020-08-24 12:29:07 +0100

    Add licensing information - EPL v1.0

--- project.clj
+++ project.clj
@@ -1,12 +1,14 @@
 (defproject server-keysets (if-let [build-num (System/getenv "CIRCLE_BUILD_NUM")]
                              (str "0.1." build-num)
                              "0.1.0-SNAPSHOT")
+  :description "CLI wrapper for circleci/clj-keyczar"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [circleci/clj-keyczar "0.1.3"]]
   :jvm-opts ["-XX:-OmitStackTraceInFastThrow"]
   :main server-keysets.core
-  :profiles {:uberjar {:aot :all
-                       #_#_:jvm-opts ["-Dclojure.compiler.direct-linking=true"]}
+  :profiles {:uberjar {:aot :all}
              :dev {:plugins [[lein-shell "0.5.0"]]}}
   :repl-options {:init-ns server-keysets.core}
   :global-vars {*warn-on-reflection* true}
